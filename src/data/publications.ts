dbf92adf7198a35bc504990d94615725c5229698
ramen-root <218883712+ramen-root@users.noreply.github.com>
2026-03-06 08:40:36 -0600

    feat: Se añadió un nuevo botón 'Código fuente', puedes colocar el enlace a github desde src/data/publications.ts, se añadió un enlace a un sitio web para DOI. El abstrac puede tener saltos de línea

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -10,12 +10,13 @@ export interface Publication {
   year: string;
   type: { en: string; es: string };
   category: string;
-  doi: string;
+  doi?: string;
   citations?: string;
   citation?: string;
   image: string;
   tags?: { en: string[]; es: string[] };
   pdf?: string;
+  sourceCode?: string;
   available: boolean;
 }
 
@@ -28,8 +29,8 @@ export const publications: Publication[] = [
     },
     pdf: "/pdf/publication/portia.pdf",
     abstract: {
-      en: "The principal objective of neural rehabilitation therapies is helping affected people to recover their mobility and reduce their dependency to other people in personal and occupational life. The way neural rehabilitation therapies are applied used to be based on the experience of the therapists and the epidemiological data available. Meanwhile, computer games (serious games), specially, based on virtual reality, are already used to treat exclusively certain types of phobia considering that an effective therapy consists on exposing patients to the source of their pathological fear within a controlled and safe environment. At present, at National Autonomous University of Mexico we are developing a set of applications based on videogames technology, programming them by the Unity SDK. The idea is helping patients to recover their mobility, which was lost by a neurological accident, or to confront their phobia. In this work we present the corresponding advances. In the case of neural rehabilitation, we focus the applications for ocular, head and neck recovery, developing some 3D scenarios for the Oculus Rift device. Respect to phobia treatment we consider attending arachnophobia, acrophobia and aviophobia, developing some 3D scenarios for Card Boards and also the Oculus Rift.",
-      es: "El objetivo principal de las terapias de rehabilitación neurológica es ayudar a las personas afectadas a recuperar su movilidad y reducir su dependencia de otras personas en su vida personal y laboral. La forma en que se aplicaban las terapias de rehabilitación neurológica solía basarse en la experiencia de los terapeutas y en los datos epidemiológicos disponibles. Por otra parte, los juegos de computadora (serious games), especialmente aquellos basados en realidad virtual, ya se utilizan para tratar exclusivamente ciertos tipos de fobias, considerando que una terapia efectiva consiste en exponer a los pacientes a la fuente de su miedo patológico dentro de un entorno controlado y seguro. Actualmente, en la Universidad Nacional Autónoma de México estamos desarrollando un conjunto de aplicaciones basadas en tecnología de videojuegos, programadas mediante el SDK de Unity. La idea es ayudar a los pacientes a recuperar la movilidad perdida debido a un accidente neurológico, o bien, a enfrentar su fobia. En este trabajo presentamos los avances correspondientes. En el caso de la rehabilitación neurológica, enfocamos las aplicaciones en la recuperación ocular, de cabeza y cuello, desarrollando algunos escenarios 3D para el dispositivo Oculus Rift. Con respecto al tratamiento de fobias, consideramos atender la aracnofobia, la acrofobia y la aviophobia, desarrollando escenarios 3D tanto para Cardboard como para Oculus Rift."
+      en: "The principal objective of neural rehabilitation therapies is helping affected people to recover their mobility and reduce their dependency to other people in personal and occupational life. The way neural rehabilitation therapies are applied used to be based on the experience of the therapists and the epidemiological data available. Meanwhile, computer games (serious games), specially, based on virtual reality, are already used to treat exclusively certain types of phobia considering that an effective therapy consists on exposing patients to the source of their pathological fear within a controlled and safe environment. At present, at National Autonomous University of Mexico we are developing a set of applications based on videogames technology, programming them by the Unity SDK. The idea is helping patients to recover their mobility, which was lost by a neurological accident, or to confront their phobia. In this work we present the corresponding advances. In the case of neural rehabilitation, we focus the applications for ocular, head and neck recovery, developing some 3D scenarios for the Oculus Rift device. Respect to phobia treatment we consider attending arachnophobia, acrophobia and aviophobia, developing some 3D scenarios for Card Boards and also the Oculus Rift. \n We thank to the Dirección General de Asuntos del Personal Académico of the National Autonomous University of Mexico, through PAPIME for the support of the Project PE104416 _Ambientes Virtuales y Herramientas Digitales para Neurociencias_ and PAPIIT for the support of the Project IT101917 _Realidad Virtual en la Visualización de Información Geográfica y Geofísica._",
+      es: "El objetivo principal de las terapias de rehabilitación neurológica es ayudar a las personas afectadas a recuperar su movilidad y reducir su dependencia de otras personas en la vida personal y laboral. La forma en que se aplicaban las terapias de rehabilitación neurológica solía basarse en la experiencia de los terapeutas y en los datos epidemiológicos disponibles. Mientras tanto, los videojuegos (juegos serios), especialmente aquellos basados en realidad virtual, ya se utilizan para tratar exclusivamente ciertos tipos de fobia, considerando que una terapia efectiva consiste en exponer a los pacientes a la fuente de su miedo patológico dentro de un entorno controlado y seguro. Actualmente, en la Universidad Nacional Autónoma de México, estamos desarrollando un conjunto de aplicaciones basadas en la tecnología de videojuegos, programándolas con el SDK de Unity. La idea es ayudar a los pacientes a recuperar su movilidad, que se perdió debido a un accidente neurológico, o a confrontar su fobia. En este trabajo presentamos los avances correspondientes. En el caso de la rehabilitación neurológica, nos enfocamos en aplicaciones para la recuperación ocular, de cabeza y cuello, desarrollando algunos escenarios en 3D para el dispositivo Oculus Rift. Respecto al tratamiento de fobias, consideramos atender la aracnofobia, la acrofobia y la aviofobia, desarrollando algunos escenarios en 3D para Card Boards y también para el Oculus Rift. \nAgradecemos a la Dirección General de Asuntos del Personal Académico de la Universidad Nacional Autónoma de México, a través de PAPIME, por el apoyo del Proyecto PE104416 _Ambientes Virtuales y Herramientas Digitales para Neurociencias_, y a PAPIIT por el apoyo del Proyecto IT101917 _Realidad Virtual en la Visualización de Información Geográfica y Geofísica._"
     },
     authors: ["D. Vargas-Herrera", "L. A. Oropeza", "Emilio Cabrera", "I. Caldelas", "F. Brambila-Paz", "R. Montúfar-Chaveznava"],
     year: "2019",
@@ -38,7 +39,8 @@ export const publications: Publication[] = [
       es: "Artículo de conferencia"
     },
     category: "Educación",
-    doi: "10.1234/rmte.2024.001",
+    doi: "https://link.springer.com/chapter/10.1007/978-3-030-16785-1_4",
+    sourceCode: "https://github.com/LIDSOL/portia/tree/unity-2017.1",
     citation: `@InProceedings{10.1007/978-3-030-16785-1_4,
 author="Vargas-Herrera, D.
 and Oropeza, L. A.
@@ -73,8 +75,8 @@ isbn="978-3-030-16785-1"
       },
       pdf: "pdf/publication/ruano-2017.pdf",
       abstract: {
-        en: "The Tor network is used to protect users' data privacy and can function as a shield to safeguard personal information.",
-        es: "La red Tor es usada para proteger la privacidad de los datos de los usuarios y puede funcionar como un escudo para proteger la información personal."
+        en: "The Tor network is used to protect users' data privacy and can act as a shield to protect personal information. \n_This article was developed in connection with the [PE102718 PAPIME/DGAPA/UNAM](https://priv-anon.unam.mx/) project._  \n_We thank DGAPA-UNAM for their support in the development of this material for PAPIME PE102718 Development of teaching materials for privacy and anonymity mechanisms in networks._",
+        es: "La red Tor es usada para proteger la privacidad de los datos de los usuarios y puede funcionar como un escudo para proteger la información personal. \n_Este artículo se desarrolló vinculado con el proyecto [PE102718 PAPIME/DGAPA/UNAM](https://priv-anon.unam.mx/)._ \n_Se agradece el apoyo otorgado para el desarrollo de este material a DGAPA-UNAM, PAPIME PE102718 Desarrollo de materiales didácticos para los mecanismos de privacidad y anonimato en redes._"
       },
       authors: ["Marco Ruano"],
       year: "2017",
@@ -83,7 +85,6 @@ isbn="978-3-030-16785-1"
         es: "Artículo de revista"
       },
       category: "Seguridad",
-      doi: "10.1234/jcsr.2024.042",
     citation: `@article{ruano_munoz_2017,
 title={La red Tor como elemento de privacidad en nuestras vidas},
 url={https://revista.seguridad.unam.mx/numero30/la-red-tor-como-elemento-de-privacidad-en-nuestras-vidas},
