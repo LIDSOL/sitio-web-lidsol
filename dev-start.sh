3742d7f28a87e7e7226495966a0b619d757529bd
ramen-root <218883712+ramen-root@users.noreply.github.com>
2026-06-05 20:03:34 -0600

    chore: disable automatic script execution. Use 'pnpm run build:blog' to manually build blog content.

--- dev-start.sh
+++ dev-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pnpm install
+pnpm install --ignore-scripts
 pnpm run build
 podman rm -f lidsol-react 2>/dev/null || true
 
@@ -8,6 +8,6 @@ podman run -d --name lidsol-react -p 8080:80 \
   -v "$(pwd)/build:/usr/share/nginx/html:ro" \
   docker.io/library/nginx:alpine
 
-while inotifywait -r -e modify,create,delete --exclude 'node_modules|build' src/ content/; do
+while inotifywait -r -e modify,create,delete --exclude 'node_modules|build' src/; do
     pnpm run build
 done
